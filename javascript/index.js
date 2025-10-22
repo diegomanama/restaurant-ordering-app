8585c22ecbb7c8e045d4ef019613c3b6d01e8709
Diego Manama <129901365+diegomanama@users.noreply.github.com>
2025-10-22 17:51:55 -0400

    refactor: stack DOM references in the top for better readability

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,14 @@
+// DOM element references
 const checkOrderBtn = document.querySelector("#check-order-button");
 const closeOrderBtn = document.querySelector("#close-order-btn");
+const completeOrderBtn = document.querySelector("#complete-order-btn");
 const orderModal = document.querySelector("#order-modal");
+const paymentModal = document.querySelector("#payment-modal");
+const closePaymentBtn = document.querySelector("#close-payment-btn");
+const payButton = document.querySelector("#pay-button");
 const nonModalContent = document.querySelector(":not(:modal)");
 
+// Event listeners
 checkOrderBtn.addEventListener("click", () => {
   orderModal.showModal();
   nonModalContent.classList.toggle("blur");
@@ -13,16 +19,10 @@ closeOrderBtn.addEventListener("click", () => {
   nonModalContent.classList.toggle("blur");
 });
 
-const completeOrderBtn = document.querySelector("#complete-order-btn");
-
 completeOrderBtn.addEventListener("click", () => {
   orderModal.close();
   paymentModal.showModal();
 });
-
-const paymentModal = document.querySelector("#payment-modal");
-const closePaymentBtn = document.querySelector("#close-payment-btn");
-const payButton = document.querySelector("#pay-button");
 
 closePaymentBtn.addEventListener("click", () => {
   paymentModal.close();
