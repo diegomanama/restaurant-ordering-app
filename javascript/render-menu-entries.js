0418a21ea61323fd0cca3b2fc62ef470fd8f7455
Diego Manama <129901365+diegomanama@users.noreply.github.com>
2025-10-30 17:34:26 -0400

    fix(menu): add missing module charged of render menu entries
    I overlooked to added it in the previous commit

---
+++ javascript/render-menu-entries.js
@@ -0,0 +1,24 @@
+import { menuArray as menuItems } from "./data.js";
+
+const menu = document.querySelector("#menu");
+const menuEntriesHTML = menuItems
+  .map((item) => {
+    return `
+        <div class="menu-entry">
+            <article class="menu-item">
+                <div class="item-image">${item.emoji}</div>
+                <div class="item-data">
+                    <h2 class="item-name">${item.name}</h2>
+                    <p class="ingredients">${item.ingredients}</p>
+                    <p class="price">$${item.price}</p>
+                </div>
+            </article>
+            <button type="button" class="add-item-button">
+                <ion-icon name="add" class="add-item-button-icon"></ion-icon>
+            </button>
+        </div> 
+    `;
+  })
+  .join("");
+
+menu.innerHTML = menuEntriesHTML;
