a9ade0ed1d5ebc0eee1b2fbc529497fb97b8e642
Diego Manama <129901365+diegomanama@users.noreply.github.com>
2026-03-25 20:59:47 -0400

    feat: enhance check order button styling and behavior
    - Hide the button off-screen by default.
    - Add animation when the button is activated so it slides in from the bottom of the window.
    - Hide the button while a modal is open for a cleaner background.

--- javascript/handle-check-order-button-state.js
+++ javascript/handle-check-order-button-state.js
@@ -1,6 +1,26 @@
-import { checkOrderButton, main } from "./shared-dom-refs.js";
+import {
+  checkOrderButton,
+  main,
+  modals,
+  orderTableBody,
+} from "./shared-dom-refs.js";
+import { addEventListenerAll } from "../utils/addEventListenerAll.js";
 
 main.addEventListener("click", () => {
   checkOrderButton.disabled = false;
   checkOrderButton.classList.add("enabled-button");
+});
+
+addEventListenerAll(modals, "toggle", () => {
+  if (modals.some((modal) => modal.open)) {
+    checkOrderButton.disabled = true;
+    checkOrderButton.classList.remove("enabled-button");
+  }
+  if (
+    !modals.some((modal) => modal.open) &&
+    orderTableBody.children.length > 0
+  ) {
+    checkOrderButton.disabled = false;
+    checkOrderButton.classList.add("enabled-button");
+  }
 });
